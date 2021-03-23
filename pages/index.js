ce0a2094de6478468a7455c41ae55b463aadeefa
Brian Kiernan <brian@briankiernan.com>
2021-03-23 16:36:06 -0400

    fix test

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>Welcome to Next.js Inside Docker!!</h1>
+        <h1 className={styles.title}>Welcome to Next.js Inside Docker!!!</h1>
 
         <p className={styles.description}>
           Get started by editing{' '}
